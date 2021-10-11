a4063f8b488d3f75ddb141df948732a43f7a3200
Ewelina Mazurek <ewelina.laszczak7@gmail.com>
2021-10-11 12:19:24 +0200

    Add List Module for LazyLoading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ListComponent } from './list/list.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 const routes: Routes = [
   {path:'', pathMatch: 'full', component: WelcomePageComponent},
-  {path:'rating', component: ListComponent},
+  {path:'rating', loadChildren: () => import('./list/list.module').then(m => m.ListModule)},
   { path: '**', pathMatch: 'full',
         component: PageNotFoundComponent }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './header/header.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
-import { ListComponent } from './list/list.component';
-import { ItemComponent } from './item/item.component';
 import {AppRoutingModule} from './app-routing.module';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     WelcomePageComponent,
-    ListComponent,
-    ItemComponent,
     PageNotFoundComponent
   ],
   imports: [
